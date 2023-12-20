c6c3b8a73cfb2ef09d59c0f6732a76158320d212
Syed Sybtain <115772979+SyedSybtain@users.noreply.github.com>
2023-12-20 17:20:34 +0500

    9 Palindrome.py

---
+++ 9 Palindrome.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        str_x = str(x)
+        rev_x = reversed(str_x)
+        rev_x = ''.join(rev_x)
+
+        if str_x == rev_x:
+            return True
+        else:
+            return False
+
+# -----------without string conversion--------------
+
+        # if x < 0:
+        #     return False
+        # initial = 0
+        # while x>initial:
+        #     initial = initial*10 + x % 10
+        #     x = x//10
+        # return x == initial or x == initial//10
