4f7dd4fbc2d9feba72513be23ac6dff4364a781a
Syed Sybtain <115772979+SyedSybtain@users.noreply.github.com>
2024-01-09 21:13:07 +0500

    Create Search a 2D Matrix.py

---
+++ Problems/Search a 2D Matrix.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        for row in matrix:
+            for element in row:
+                if target == element:
+                    return True
+        return False
