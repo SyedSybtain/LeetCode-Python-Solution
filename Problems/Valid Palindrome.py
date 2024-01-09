630218be8d213a0a6fa9cfae8cf2fbbd4db04b69
Syed Sybtain <115772979+SyedSybtain@users.noreply.github.com>
2024-01-09 21:12:16 +0500

    Create Valid Palindrome.py

---
+++ Problems/Valid Palindrome.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s = s.lower()
+        new_s = ''
+        for i in s:
+            if (ord(i) >=97) and (ord(i)<=122):
+                new_s += i
+            if (ord(i) >=47) and (ord(i)<=57):
+                new_s += i
+        s = new_s[::-1]
+        print(new_s,s)
+        if new_s == s:
+            return True
+        else:
+            return False
