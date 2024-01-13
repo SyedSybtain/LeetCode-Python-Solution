79e732f79ea065625828544f4cc9eea1ff62c89c
Syed Sybtain <115772979+SyedSybtain@users.noreply.github.com>
2024-01-13 20:24:16 +0500

    Create Multiply String.py

---
+++ Programming Skills/Multiply String.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        num1 = int(num1)
+        num2 = int(num2)
+        mul = num1*num2
+        return str(mul)
